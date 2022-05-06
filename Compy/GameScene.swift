d8ee4feb74d237d1cfd71d313b08c979fc1dc1f7
masxxiii <masxafar@gmail.com>
2022-05-06 19:25:03 +0700

    init

---
+++ Compy/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Compy
+//
+//  Created by Masood Zafar on 06.05.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
