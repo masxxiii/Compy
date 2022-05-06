18e9f32a440526e99968769370d200da421d6565
masxxiii <masxafar@gmail.com>
2022-05-06 22:50:29 +0700

    implement gameSprite

--- Compy/Classes/Compy.swift
+++ Compy/Classes/Compy.swift
@@ -8,5 +8,9 @@
 import SpriteKit
 
 class Compy: SKSpriteNode, GameSprite {
-
+    var textureAtlas: SKTextureAtlas = SKTextureAtlas(named: "Compy")
+    var initialSize: CGSize = CGSize(width: 100, height: 100)
+    
+    // function for adding tap functionality.
+    func onTap() {}
 }
