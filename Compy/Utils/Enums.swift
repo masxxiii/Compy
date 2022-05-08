16e196774c36c3eb091e1630d6a74c025a801dfb
masxxiii <masxafar@gmail.com>
2022-05-09 01:51:12 +0700

    add utils group

---
+++ Compy/Utils/Enums.swift
@@ -0,0 +1,17 @@
+//
+//  Enums.swift
+//  Compy
+//
+//  Created by Masood Zafar on 09.05.2022.
+//
+
+import Foundation
+
+enum PhysicsCategory: UInt32 {
+    case Compy = 1
+    case DamagedCompy = 2
+    case Ground = 4
+    case Alien = 8
+    case Droid = 16
+    case Powerup = 32
+}
