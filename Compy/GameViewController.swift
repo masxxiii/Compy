e0f8a0c43a07329b6989329a03d1e8253d4cc065
masxxiii <masxafar@gmail.com>
2022-05-08 17:43:11 +0700

    remove main storyboard

--- Compy/GameViewController.swift
+++ Compy/GameViewController.swift
@@ -11,26 +11,28 @@ import GameplayKit
 
 class GameViewController: UIViewController {
     
-    override func viewWillLayoutSubviews() {
-        
-        super.viewWillLayoutSubviews()
-        
-        // creates our scene
-        let scene = GameScene()
-        
-        // configuring
-        let skView = self.view as! SKView
-        skView.showsFPS = true
-        skView.showsNodeCount = true
-        skView.ignoresSiblingOrder = true
-        
-        // setting the scale mode:
-        scene.scaleMode = .aspectFill
-        // size our scene to fit the view exactly:
-        scene.size = view.bounds.size
-        // show the new scene:
-        skView.presentScene(scene)
-    }
+    override func viewDidLoad() {
+         super.viewDidLoad()
+
+         view = SKView(frame: view.bounds)
+
+         if let view = self.view as! SKView? {
+             // Initialise the scene
+             let scene = GameScene(size: view.bounds.size)
+
+             // Set the scale mode to scale to fit the window
+             scene.scaleMode = .aspectFill
+
+             // Present the scene
+             view.presentScene(scene)
+
+             // Scene properties
+             view.showsPhysics = false
+             view.ignoresSiblingOrder = true
+             view.showsFPS = true
+             view.showsNodeCount = true
+         }
+     }
 
     override var shouldAutorotate: Bool {
         return true
